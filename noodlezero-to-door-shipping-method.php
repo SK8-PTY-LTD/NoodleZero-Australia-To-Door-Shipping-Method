380c409994a3a18393ed7be92d9f293da51b90fa
Jack Song <133342301@163.com>
2018-02-02 16:47:44 +1100

    Added WA & SA

--- noodlezero-to-door-shipping-method.php
+++ noodlezero-to-door-shipping-method.php
@@ -51,6 +51,8 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 					$this->sydzip = isset($this->settings['sydzip']) ? $this->settings['sydzip'] : __('2000', 'noodlezero_to_door');
 					$this->melzip = isset($this->settings['melzip']) ? $this->settings['melzip'] : __('3000', 'noodlezero_to_door');
 					$this->brizip = isset($this->settings['brizip']) ? $this->settings['brizip'] : __('4000', 'noodlezero_to_door');
+					$this->perzip = isset($this->settings['perzip']) ? $this->settings['perzip'] : __('4000', 'noodlezero_to_door');
+					$this->adlzip = isset($this->settings['adlzip']) ? $this->settings['adlzip'] : __('4000', 'noodlezero_to_door');
 					// New Zealand
 					$this->nzmiddleearthisland = isset($this->settings['nzmiddleearthisland']) ? $this->settings['nzmiddleearthisland'] : __('4000', 'noodlezero_to_door');
 					$this->nznorthisland = isset($this->settings['nznorthisland']) ? $this->settings['nznorthisland'] : __('4000', 'noodlezero_to_door');
@@ -125,6 +127,20 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 							'default' => "4000",
 						),
 
+						'perzip' => array(
+							'title' => __('Perth ZIPs', 'noodlezero_to_door'),
+							'type' => 'text',
+							'description' => __('Use , to seperate', 'noodlezero_to_door'),
+							'default' => "4000",
+						),
+
+						'adlzip' => array(
+							'title' => __('Adlaide ZIPs', 'noodlezero_to_door'),
+							'type' => 'text',
+							'description' => __('Use , to seperate', 'noodlezero_to_door'),
+							'default' => "4000",
+						),
+
 						'nzmiddleearthisland' => array(
 							'title' => __('NZ Middle Earth ZIPs', 'noodlezero_to_door'),
 							'type' => 'text',
@@ -191,6 +207,8 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 					if (strstr($this->sydzip, $postcode) === FALSE &&
 						strstr($this->melzip, $postcode) === FALSE &&
 						strstr($this->brizip, $postcode) === FALSE &&
+						strstr($this->perzip, $postcode) === FALSE &&
+						strstr($this->adlzip, $postcode) === FALSE &&
 						$country == "AU") {
 						// The Shipping post code is not found in the pre-configured zip area.
 						// To Door shipping not available
@@ -210,9 +228,7 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 
 					$quantity = WC()->cart->get_cart_contents_count();
 
-//				echo '<pre> Weight Total Before', $weight, '</pre>';
 					$weight = wc_get_weight($weight, 'kg');
-//                echo '<pre> Weight Total After', $weight, '</pre>';
 
 					if ($quantity >= $this->combo) {
 
